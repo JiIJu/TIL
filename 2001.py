d69bbdea1a021971906083b14f0428b54ad84d6f
JiIJu <eldk313131@gmail.com>
2022-07-18 13:57:06 +0900

    좀 돼라

---
+++ 2001.py
@@ -0,0 +1,18 @@
+'''T = int(input())
+
+for a in (1,T+1):
+    N,M = map(int,input().split())
+    data = [list(map(int,input().split())) for _ in range(N)]
+
+    for i in range(N):
+        n = i
+        m = 0
+        total = 0
+        max = 0
+        while m<M:
+            total += data[n][m]'''
+print(3.2-3.1)
+print("철수 \'안녕\'\n\'안녕\'")
+name = "마니"
+score = 100
+print(f'hello {name}! 너의 성적은 {score}점이야')
