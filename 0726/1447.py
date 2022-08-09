a48e995ee3f51847876b052c3f8552c617e7d596
JiIJu <109330668+JiIJu@users.noreply.github.com>
2022-08-09 10:56:49 +0900

    Delete 0726 directory

--- 0726/1447.py
+++
@@ -1,9 +0,0 @@
-def de_identify(number):
-    number = list(number)
-    for i in range(6,len(number)):
-        number[i] = '*'
-    a = ''
-    for i in range(len(number)):
-        a+=f'{number[i]}'
-    return a
-print(de_identify('970103-1234567'))
--- 0726/1449.py
+++
@@ -1,9 +0,0 @@
-
-def count_vowels(word):
-    vowels = 'aeiou'
-    result = 0
-    for vowel in vowels:
-        result += word.count(vowel)
-    return result
-
-print(count_vowels('apple'))
