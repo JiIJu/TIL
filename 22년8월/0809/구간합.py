60758c707f39ccb103a7ff75c7c9f95ccf1b26f9
JiIJu <109330668+JiIJu@users.noreply.github.com>
2022-08-09 17:45:06 +0900

    교수님 풀이 업로드

--- 22년8월/0809/구간합.py
+++ 22년8월/0809/구간합.py
@@ -13,3 +13,25 @@ for a in range(1,T+1):
             if data[j] > data[j+1]:
                 data[j] , data[j+1] = data[j+1] , data[j]
     print(f'#{a} {data[-1]-data[0]}')
+
+#교수님 풀이 업로드
+
+T = int(input())
+
+for tc in range(1,T+1):
+    N,M = map(int,input().split())
+    numbers = list(map(int,input().split()))
+    
+    #구간합의 최소,최대값
+    max_sum = 0
+    min_sum = 10000 * M
+    
+    for i in range(0,N-M+1):
+        sub_sum = 0 #구간 합
+        # M만큼 가면서 구간합을 구해준다.
+        for j in range(0,M):
+            sub_sum += numbers[i+j]
+        max_sum = sub_sum if sub_sum>max_sum else max_sum
+        min_sum = sub_sum if sub_sum>min_sum else min_sum
+    print(f'#{tc} {max_sum-min_sum}
+    
