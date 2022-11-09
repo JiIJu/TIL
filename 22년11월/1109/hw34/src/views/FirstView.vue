41a6d206a6c7d355216cf2221a1b0b93c1d5afc6
chaeeun5 <jce0497@gmail.com>
2022-11-09 17:52:50 +0900

    vue router

---
+++ 22년11월/1109/hw34/src/views/FirstView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HelloWorld
+    :hello = 'hello'
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+export default {
+  name : 'FirstView',
+  components: {
+    HelloWorld,
+  },
+  computed:{
+    hello() {
+      return '첫 번째 페이지입니다.'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
