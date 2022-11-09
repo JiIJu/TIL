7fdc598211ea329db5e570a7afd7326b5e4395ee
chaeeun5 <jce0497@gmail.com>
2022-11-09 16:58:43 +0900

    vue router

---
+++ 22년11월/1109/vue-router-app/src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{name : 'home'}">집으로!</router-link>
+    <button @click="toHome">홈으로</button>
+    <input type="text" @keyup.enter="goHello" v-model="hellotext">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name : 'AboutView',
+  methods: {
+    toHome() {
+      this.$router.push({ name : 'home'})
+    },
+    goHello() {
+      this.$router.push({name:'hello' , params:{userName : this.hellotext}})
+    }
+  },
+  data()  {
+    return {
+      hellotext : null,
+    }
+  },
+
+
+}
+</script>
