a82eac33321d848a63673a57a8945c7f7307e31e
chaeeun5 <jce0497@gmail.com>
2022-11-14 17:42:07 +0900

    last vue

---
+++ 22년11월/1114/vue_07_workshop/client/src/views/todos/CreateTodo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model.trim="title" 
+      @keyup.enter="createTodo"
+    >
+    <button @click="createTodo">+</button>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+
+export default {
+  name: 'CreateTodo',
+  data: function () {
+    return {
+      title: null,
+    }
+  },
+  methods: {
+    createTodo: function () {
+      // 2번 문제
+      const title = this.title
+      axios({
+        method : 'post',
+        url : `http://127.0.0.1:8000/todos/`,
+        data : {
+          title : title,
+        }
+      })
+      .then((res) => {
+        console.log(res)
+        this.$router.push({name : 'TodoList'})
+        this.title = null
+      })
+      .catch((err) => {
+        console.log(err)
+        console.log(123)
+      })
+    }
+  }
+}
+</script>
